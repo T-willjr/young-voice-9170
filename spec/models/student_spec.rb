67acf37d285dd9a32513f7d6cae2e55823d217bc
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-11 16:19:15 -0500

    Test: returns #average_age of students

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe "average_age" do
+    it "returns average age" do
+      harry = Student.create(name: "Harry Potter" , age: 10 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 10 , house: "Slytherin" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 10 , house: "Gryffindor" )
+
+      expect(Student.average_age).to eq(10)
+    end
+  end
 end
